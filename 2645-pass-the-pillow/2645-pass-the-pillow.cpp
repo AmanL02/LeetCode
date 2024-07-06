983c3e7201208f65dffd600c5d2e2cd2f1cd01f0
Aman Kumar Singh <97456601+AmanL02@users.noreply.github.com>
2024-07-06 13:10:55 +0530

    Time: 0 ms (100%), Space: 7.2 MB (30.58%) - LeetHub

---
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int x=time%(n-1);
+        int y=time/(n-1);
+        if(y%2==0){
+            return x+1;
+        }
+        return n-x;
+    }
+};
